11a44b31d77843ed0c0b9275063aa46870bc815a
clcvillanueva <lambertcvillanueva@gmail.com>
2021-07-31 00:42:08 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LandingPage from 'components/LandingPage';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <LandingPage />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
